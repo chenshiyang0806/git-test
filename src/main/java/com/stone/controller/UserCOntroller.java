85c68d6f61fa60cdb43e306efc55eed1a8f1c0eb
chenshiyang <chenshiyang0806@163.com>
2020-08-10 16:53:52 +0800

    第2次提交

---
+++ src/main/java/com/stone/controller/UserCOntroller.java
@@ -0,0 +1,13 @@
+package com.stone.controller;
+
+/**
+ * @description:
+ * @author: ccssyy
+ * @time: 2020/8/10 16:52
+ */
+public class UserController {
+
+    public void findUser(String id){
+        System.out.println("查找用户");
+    }
+}
